931012a4f02a8d7a6113305ea9ede5f86c092e1e
Sorawis Leepiyasakulchai <sorawisson@gmail.com>
2025-05-10 22:35:01 +0700

    edit trashcanUI

--- Assets/Scripts/Inventory System/UI/TrashCanUI.cs
+++ Assets/Scripts/Inventory System/UI/TrashCanUI.cs
@@ -1,15 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class TrashCanUI : MonoBehaviour, IDropHandler
 {
-    
-    [SerializeField] private Transform TargetPoint; // Optional, use trashcan center if null
-    [SerializeField] private float Duration = 0.3f;
-    
     public void OnDrop(PointerEventData eventData)
     {
         if (InventorySlotUI.HasActiveDrag())
